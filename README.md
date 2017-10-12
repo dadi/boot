af3a05e6c4368f57b64c9144446b7a377dee46c1
David Longworth <dave@abovebored.co.uk>
2017-10-12 11:28:19 +0100

    🌅

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# dadi-boot
-A module to help DADI apps boot uniformly.
+# DADI Boot
+> A module to help DADI apps boot uniformly.
+
+## Usage
+
+### Starting an app
+
+In the file which is triggered by `npm start` e.g., `main.js`
+
+```js
+const appName = require('./package.json').description
+const dadiBoot = require('@dadi/boot')
+
+// Console start message
+dadiBoot.start(appName)
+```
+
+`appName` looks at the `description` field in the `package.json` file, this should be the brand name for the app e.g., `DADI Web`
+
+### Successfully started
+
+Located where the app finishes its initialisation process.
+
+```js
+const dadiBoot = require('@dadi/boot')
+
+dadiBoot.started({
+  server: `${config.get('server.protocol')}://${config.get('server.host')}:${config.get('server.port')}`,
+  header: {
+    app: config.get('app.name')
+  },
+  body: {
+    'Protocol': config.get('server.protocol'),
+    'Version': version,
+    'Node.js': nodeVersion,
+    'Engine': enginesInfo,
+    'Environment': config.get('env')
+  },
+  footer: {
+    'DADI API': config.get('api.enabled') ? `${config.get('api.host')}:${config.get('api.port')}` : '\u001b[31mNot enabled\u001b[39m'
+  }
+})
+```
+
+> N.B. All fields are optional
+
+#### `server`
+
+The location where the app is launched or available for the user to be interacted with.
+
+#### `header`
+
+An object of strings to put in the header.
+
+> N.B. The object keys are not currently used
+
+#### `body`
+
+An object of strings to output into a key|value table
+
+#### `footer` 
+
+An object of supplementary information which the user might need to know, e.g., status of other connected microservices
+
+### Errors
+
+A string which is outputted as red in the terminal. Always ends with a process kill `process.exit(0)`.
+
+```js
+dadiBoot.error(err)
+```
+
+### Stopped
+
+To be triggered anytime the app is shutdown. A string which is outputted as red in the terminal. Always ends with a process kill `process.exit(0)`.
+
+```js
+dadiBoot.stopped()
+```
---
+++ index.js
@@ -0,0 +1,94 @@
+const logo = `
+   ▓▓▓▓▓  ▓▓▓▓▓▓▓
+               ▓▓▓▓
+      ▓▓▓▓▓▓▓    ▓▓▓▓
+               ▓▓▓▓
+          ▓▓▓▓▓▓▓`
+
+const docs = 'https://docs.dadi.tech'
+
+const cliTable = require('cli-table2')
+const colors = require('colors')
+const concierge = require('ora')()
+
+// Make some tables we can use to pretty print data
+
+const header = new cliTable({
+  colWidths: [42],
+  chars: { 'bottom': '', 'bottom-mid': '', 'bottom-left': '', 'bottom-right': '', 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' },
+  style: { 'padding-left': 1, 'border': ['grey'], head: []},
+  wordWrap: true
+})
+
+const body = new cliTable({
+  colWidths: [14, 27],
+  chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''},
+  style: { 'padding-left': 1, 'border': ['grey'] },
+  wordWrap: true
+})
+
+const footer = new cliTable({
+  colWidths: [14, 27],
+  chars: { 'top': '', 'top-mid': '', 'top-left': '', 'top-right': '' },
+  style: { 'padding-left': 1, 'border': ['grey'], head: []},
+  wordWrap: true
+})
+
+/**
+ * Adds a loading spinner to the cli with the product name
+ * @param {product} product - The name of the app, passed from the app's package.json
+ */
+
+let appName // Make this globally available
+
+module.exports.start = product => {
+  appName = product
+  concierge.start(`Starting ${appName}…`)
+  concierge.color = 'white'
+}
+
+/**
+ * If the app starts successfully this prints a welcome message
+ * @object {info} info - An object of info to tell the user (where the app is, node versions etc)
+ */
+
+module.exports.started = info => {
+  Object.keys(info.header).map(i => header.push([info.header[i]]))
+  Object.keys(info.body).map(i => body.push([i.green, info.body[i]]))
+  Object.keys(info.footer).map(i => footer.push([i, info.footer[i]]))
+
+  concierge.succeed(`Started ${appName}\n${`@`.green} ${info.server.underline}
+  
+  ${header.toString().split('\n').join('\n  ')}
+  ${body.toString().split('\n').join('\n  ')}
+  ${footer.toString().split('\n').join('\n  ')}
+
+  ${`ℹ`.green} Documentation at ${docs}
+  ${`ℹ`.green} <CTRL> + C to shut down
+
+  ${logo}
+
+
+  ${`© ${new Date().getFullYear()} DADI+ Limited (https://dadi.tech)\n  All rights reserved.`.grey}
+  `)
+}
+
+/**
+ * Prints an exit error and kills the process
+ * @param {err} error - A text description of the error
+ */
+
+module.exports.error = err => {
+  concierge.fail(`${err}`.red)
+  process.exit(0)
+}
+
+/**
+ * Prints a shutdown message and kills the process
+ * @param {err} error - A text description of the error
+ */
+
+module.exports.stopped = message => {
+  concierge.fail(`Stopping & exiting ${appName}.`.red)
+  process.exit(0)
+}
